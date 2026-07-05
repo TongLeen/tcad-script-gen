c0cd7b0c78f1a41bc26003cf49387977fb54b312
TongLeen <Tong.Leen@outlook.com>
2026-07-05 17:38:26 +0800

    [add] 新增 Sdevice 的 Math

---
+++ src/sentaurus/sdevice/math.ts
@@ -0,0 +1,74 @@
+type MathType = {
+
+}
+
+type ConvergenceControl = {
+    RHSMin?: number
+    RHSMinFactor?: number
+    Digits?: number
+    ErrRef?: ErrRef
+    RHSFactor?: number
+    RHSMax?: number
+    CheckBehavior?: "CheckRhsAfterUpdate"
+}
+
+type ErrRef = {
+    Electron?: number
+    Hole?: number
+}
+
+type ComputationControl = {
+    FailureBehavior?: "ExitOnFailure"
+    Transient?: "BE" | "TRBDF"
+    Extrapolate?: true | Extrapolate
+    Iterations?: number
+    Threads?: number | { Assembly: number, Solver: number }
+    Method?: Method
+}
+
+type Extrapolate = {
+    Algorithm?: 1 | 2
+    Exclude?: never
+    LowDensityLimit?: number
+    MinStep?: number
+    NumberOfFailures?: number
+    Order?: number
+}
+
+type LinearSolver = "Super" | "ParDiSo" | "ILS"
+type Method = LinearSolver | {
+    main: "Blocked"
+    sub: LinearSolver
+}
+
+type ValueControl = {
+    DirectCurrentComputation?: true
+    ExtendedPrecision?: true | 64 | 80 | 128 | 256
+    Derivatives?: boolean
+    AvalDerivatives?: boolean
+    RefDens_eGradQuasiFermi_ElectricField?: number
+}
+
+type NonlocalConfig = {
+    Electrode: string
+    Digits: number
+    Length: number
+    EnergyResolution: number
+    Direction: [number, number, number]
+    MaxAngle: number
+    EndPoints: EndPoint[]
+}
+type EndPoint = {
+    enable: boolean
+    material: string
+}
+
+const mathGenerator = (ctx: string[]) => {
+    const math = (m: MathType) => {
+
+    }
+    return math;
+}
+
+export { mathGenerator };
+export type { MathType };
