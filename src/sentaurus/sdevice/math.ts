0989193371c165cbf3eea400414305c681104331
TongLeen <tong.leen@outlook.com>
2026-07-08 16:37:00 +0800

    [add] 完成 Math 的 formatter 实现

--- src/sentaurus/sdevice/math.ts
+++ src/sentaurus/sdevice/math.ts
@@ -1,3 +1,5 @@
+import useFormatUtils from "./format-utils"
+
 type MathType =
     & ConvergenceControl
     & ComputationControl
@@ -10,7 +12,24 @@ type ConvergenceControl = {
     ErrRef?: ErrRef
     RHSFactor?: number
     RHSMax?: number
-    CheckBehavior?: "CheckRhsAfterUpdate"
+    CheckBehavior?: "CheckRhsAfterUpdate" | "RhsAndUpdateConvergence"
+}
+
+const formatConvergenceControl = (raw: ConvergenceControl) => {
+    const retval: string[] = []
+    const { formatAssignment } = useFormatUtils(retval)
+    formatAssignment(raw, "RHSMin")
+    formatAssignment(raw, "RHSMinFactor")
+    formatAssignment(raw, "Digits")
+    formatAssignment(raw, "RHSFactor")
+    formatAssignment(raw, "RHSMax")
+    if (raw.CheckBehavior) retval.push(raw.CheckBehavior)
+    if (raw.ErrRef) {
+        const r = raw.ErrRef
+        if (r.Electron) retval.push(`ErrRef(Electron)=${r.Electron}`)
+        if (r.Hole) retval.push(`ErrRef(Hole)=${r.Hole}`)
+    }
+    return retval
 }
 
 type ErrRef = {
@@ -19,12 +38,47 @@ type ErrRef = {
 }
 
 type ComputationControl = {
-    FailureBehavior?: "ExitOnFailure"
+    ExitOnFailure?: true
     Transient?: "BE" | "TRBDF"
     Extrapolate?: true | Extrapolate
     Iterations?: number
     Threads?: number | { Assembly: number, Solver: number }
     Method?: Method
+}
+
+const formatComputationControl = (raw: ComputationControl) => {
+    const retval: string[] = []
+    const { formatFlag, formatAssignment, formatBlock } = useFormatUtils(retval)
+    formatFlag(raw, "ExitOnFailure")
+    formatAssignment(raw, "Transient")
+    formatAssignment(raw, "Iterations")
+    formatBlock(raw, "Extrapolate", (e) => {
+        if (e === true) return []
+        const l: string[] = []
+        const { formatFlag, formatAssignment, formatBlock } = useFormatUtils(l)
+        formatAssignment(e, "Algorithm")
+        formatAssignment(e, "LowDensityLimit")
+        formatAssignment(e, "MinStep")
+        formatAssignment(e, "NumberOfFailures")
+        formatAssignment(e, "Order")
+        return l
+    })
+    if (raw.Threads !== undefined) {
+        if (typeof raw.Threads === 'number') retval.push(`NumberOfThreads=${raw.Threads}`)
+        else retval.push(
+            `NumberOfAssemblyThreads=${raw.Threads.Assembly}`,
+            `NumberOfSolverThreads=${raw.Threads.Solver}`,
+        )
+    }
+    if (raw.Method !== undefined) {
+        const M = raw.Method
+        if (typeof M === 'object' && M.solver === 'Blocked') {
+            retval.push("Method=Blocked")
+            retval.push("SubMethod", "=", ...formatLinearSolver(M.sub))
+        }
+        else retval.push("Method", "=", ...formatLinearSolver(M))
+    }
+    return retval
 }
 
 type Extrapolate = {
@@ -49,9 +103,28 @@ type LinearSolver = "Super" | "ParDiSo" | "ILS"
         MultipleRHS?: boolean,
         Set?: number,
     }
+const formatLinearSolver = (s: LinearSolver) => {
+    if (typeof s === 'string') return [s]
 
+    const retval: string[] = [s.solver, "("]
+    const { formatSwitch, formatAssignment } = useFormatUtils(retval)
+    switch (s.solver) {
+        case "ParDiSo":
+            formatSwitch(s, "IterativeRefinement")
+            formatSwitch(s, "MultipleRHS")
+            formatSwitch(s, "NonsymmetricPermutation")
+            formatSwitch(s, "RecomputeNonsymmetricPermutation")
+            break
+        case "ILS":
+            formatSwitch(s, "MultipleRHS")
+            formatAssignment(s, "Set")
+            break
+    }
+    retval.push(")")
+    return retval
+}
 type Method = LinearSolver | {
-    main: "Blocked"
+    solver: "Blocked"
     sub: LinearSolver
 }
 
@@ -63,7 +136,18 @@ type ValueControl = {
     RefDens_eGradQuasiFermi_ElectricField?: number
 }
 
-type NonlocalConfig<M extends string> = {
+const formatValueControl = (raw: ValueControl) => {
+    const retval: string[] = []
+    const { formatFlag, formatBlock, formatSwitch, formatAssignment } = useFormatUtils(retval)
+    formatFlag(raw, "DirectCurrentComputation")
+    formatBlock(raw, "ExtendedPrecision", (e) => (e === true) ? [] : [`${e}`])
+    formatSwitch(raw, "AvalDerivatives")
+    formatSwitch(raw, "Derivatives")
+    formatAssignment(raw, "RefDens_eGradQuasiFermi_ElectricField")
+    return retval
+}
+
+type Nonlocal<M extends string> = {
     Electrode: string
     Digits: number
     Length: number
@@ -77,9 +161,35 @@ type EndPoint<M extends string> = {
     material: M
 }
 
-const mathGenerator = <M extends string>(ctx: string[]) => {
-    const math = (m: MathType, nonlocalmesh?: Record<string, NonlocalConfig<M>>) => {
+const formatNonlocal = <M extends string>(n: Nonlocal<M>) => {
+    const retval: string[] = []
+    const { formatAssignment } = useFormatUtils(retval)
+    formatAssignment(n, "Digits")
+    formatAssignment(n, "Electrode", (k, v) => `${k}="${v}"`)
+    formatAssignment(n, "Length")
+    formatAssignment(n, "EnergyResolution")
+    formatAssignment(n, "MaxAngle")
+    retval.push(`Direction=(${n.Direction.join(' ')})`)
+    if (n.EndPoints) {
+        n.EndPoints.map((e) => `${e.enable ? "+" : "-"}EndPoint(Material="${e.material}")`)
+    }
+    return retval
+}
 
+const mathGenerator = <M extends string>(ctx: string[]) => {
+    const math = (m: MathType, nonlocalmesh?: Record<string, Nonlocal<M>>) => {
+        const ll: string[][] = []
+        ll.push(
+            formatConvergenceControl(m),
+            formatComputationControl(m),
+            formatValueControl(m),
+        )
+        if (nonlocalmesh) {
+            Object.entries(nonlocalmesh).forEach(([k, v]) => {
+                ll.push(["NonLocal", `"${k}"`, "(", ...formatNonlocal(v), ")"])
+            })
+        }
+        ctx.push("Math", "{", ...ll.flat(), "}")
     }
     return math;
 }
