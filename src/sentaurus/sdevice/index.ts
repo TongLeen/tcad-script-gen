025fa8453122c9dde4dc0ddaa22f172cace75121
TongLeen <tong.leen@outlook.com>
2026-07-08 16:38:18 +0800

    [add] 直接拉起 sdevice

--- src/sentaurus/sdevice/index.ts
+++ src/sentaurus/sdevice/index.ts
@@ -1,7 +1,10 @@
+import { tmpdir } from "node:os"
+import { join } from "node:path"
 import { fileGenerator } from "./file"
 import { electrodeGenerator } from "./electrode"
 import { physicsGenerator } from "./physics"
 import { mathGenerator } from "./math"
+import { plotGenerator } from "./plot"
 
 const useSdevice = <M extends string, D extends string>() => {
     let cmds: string[] = []
@@ -10,13 +13,41 @@ const useSdevice = <M extends string, D extends string>() => {
         Bun.write(filename, format(cmds))
     }
 
+    const run = (filename?: string) => {
+        if (filename === undefined) {
+            const uuid = crypto.randomUUID();
+            const tmpfilepath = join(tmpdir(), uuid)
+            Bun.write(tmpfilepath, cmds.join(' '))
+            const proc = Bun.spawnSync({
+                cmd: ["sdevice", tmpfilepath],
+                stdout: 'inherit',
+                stderr: 'inherit',
+            })
+            return proc.exitCode
+        }
+        else {
+            const proc = Bun.spawnSync({
+                cmd: ["sdevice", filename],
+                stdout: 'inherit',
+                stderr: 'inherit',
+            })
+            return proc.exitCode
+        }
+    }
+
+    const runAndExit = (filename?: string) => {
+        process.exit(run(filename))
+    }
 
     return {
         file: fileGenerator(cmds),
         electrode: electrodeGenerator(cmds),
         physics: physicsGenerator<M, D>(cmds),
         math: mathGenerator<M>(cmds),
+        plot: plotGenerator(cmds),
         save,
+        run,
+        runAndExit,
     }
 }
 
