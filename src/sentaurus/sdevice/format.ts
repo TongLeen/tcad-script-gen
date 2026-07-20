574f8d28da81df2453278bea2c5d64f23ed25ab4
TongLeen <tong.leen@outlook.com>
2026-07-20 22:25:18 +0800

    [add] 新增 新的SDevice format 方法

--- src/sentaurus/sdevice/format.ts
+++ src/sentaurus/sdevice/format.ts
@@ -1,9 +1,10 @@
 type FormatBatch<T extends Record<string, unknown>> = {
-    flags?: (keyof T)[];
+    flag?: (keyof T)[];
     switch?: (keyof T)[];
     assign?: (keyof T)[];
     assignString?: (keyof T)[];
-    others?: { [K in keyof T]: (k: K, v: T[K]) => string[] };
+    block?: { [K in keyof T]?: (v: NonNullable<T[K]>) => string[] };
+    others?: { [K in keyof T]?: (k: K, v: NonNullable<T[K]>) => string[] };
 };
 
 const SDeviceFormat =
@@ -11,31 +12,54 @@ const SDeviceFormat =
     (format: FormatBatch<T>) => {
         const retval: string[] = [];
 
-        if (format.flags !== undefined) {
-            retval.push(...(format.flags as string[]));
+        if (format.flag !== undefined) {
+            const existing_flag = field_filter(entity, format.flag);
+            retval.push(...(existing_flag as string[]));
         }
         if (format.switch !== undefined) {
+            const existing_sw = field_filter(entity, format.switch);
             retval.push(
-                ...(format.switch as string[]).map(
+                ...(existing_sw as string[]).map(
                     (v) => (entity[v] ? "+" : "-") + v,
                 ),
             );
         }
         if (format.assign !== undefined) {
+            const existing_assign = field_filter(entity, format.assign);
             retval.push(
-                ...(format.assign as string[]).map((v) => `${v}=${entity[v]}`),
+                ...(existing_assign as string[]).map(
+                    (v) => `${v}=${entity[v]}`,
+                ),
             );
         }
         if (format.assignString !== undefined) {
+            const existing_assignString = field_filter(
+                entity,
+                format.assignString,
+            );
             retval.push(
-                ...(format.assignString as string[]).map(
+                ...(existing_assignString as string[]).map(
                     (v) => `${v}="${entity[v]}"`,
                 ),
             );
         }
+        if (format.block !== undefined) {
+            const r = Object.entries(format.block)
+                .filter(([k, v]) => v !== undefined && entity[k] !== undefined)
+                .map(([k, v]) => {
+                    const r: string[] = [k, "("];
+                    r.push(...v(entity[k]));
+                    r.push(")");
+                    return r;
+                });
+            retval.push(...r.flat());
+        }
         if (format.others !== undefined) {
             retval.push(
                 ...Object.entries(format.others)
+                    .filter(
+                        ([k, v]) => v !== undefined && entity[k] !== undefined,
+                    )
                     .map(([k, v]) => v(k, entity[k]))
                     .flat(),
             );
@@ -45,3 +69,8 @@ const SDeviceFormat =
     };
 
 export default SDeviceFormat;
+
+const field_filter = <T extends Record<string, unknown>>(
+    e: T,
+    fields: (keyof T)[],
+) => fields.filter((v) => e[v] !== undefined);
